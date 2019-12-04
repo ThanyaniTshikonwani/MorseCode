fc52bc82fca10f6b78b469a02d98252a190aed56
ThanyaniTshikonwani <Thanyani.Tshikonwani@gmail.com>
2019-12-04 10:04:10 +0200

    2nd Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Morse Code Encryption Program");
-        System.out.println("Press 1 To Convert letters to Morse Code");
-        System.out.println("Press 2 To Convert Morse Code to Letter");
-        int option = input.nextByte();
-        invoker(option);
+        String answer;
+            do {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Morse Code Encryption Program");
+                System.out.println("Press 1 To Convert letters to Morse Code");
+                System.out.println("Press 2 To Convert Morse Code to Letter");
+                int option = input.nextByte();
+                invoker(option);
+
+                System.out.println("To re-run please type 'Yes'or No to exit ");
+            answer=input.next();
+        } while (answer.equalsIgnoreCase("Yes"));
     }
     public static void invoker(int option){
         Scanner input2 = new Scanner(System.in);
-        String answer;
-       do {
            switch (option){
                case 1:
-//                   Scanner input = new Scanner(System.in);
                    System.out.println("Please enter a Text eg:(Hello World)");
                    String letters = input2.nextLine();
                    String result0 = MorseCode.lettersToMorseCode(letters);
@@ -23,7 +26,6 @@ public class Main {
                    System.out.println(result0);
                    break;
                case 2:
-//                   Scanner  = new Scanner(System.in);
                    System.out.println("Please enter a Morse Code");
                    String morse = input2.nextLine();
                    String result = MorseCode.morseCodeToLetters(morse);
@@ -34,9 +36,5 @@ public class Main {
                    System.out.println("Incorrect entry, Please run Again.");
                    break;
            }
-           System.out.println("To re-run please type 'Yes'or No to exit ");
-           answer=input2.next();
-       } while (answer.equalsIgnoreCase("Yes"));
        }
-
     }
